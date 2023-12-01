ec3da2f4b4a250dabd970a30db02228ecfcd21b6
Kirk Douglas <125107141+kirkdouglasouza@users.noreply.github.com>
2023-11-30 20:27:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-function MudarFundo() {
-    // Gere uma cor aleatória
-//var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 
-// Aplique a cor ao plano de fundo do body
-document.body.style.backgroundImage = "url('/image/madeira.png')";
-}
 function Filme(){
 
     //Redireciona para um filme do Mick Mouse
